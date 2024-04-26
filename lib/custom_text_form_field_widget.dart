e6207a34ef0d15680eb32a330a38e65a569dbbad
Zahra Ali Abouelkheir <88488265+zahraaliloutfi@users.noreply.github.com>
2024-04-26 18:02:24 +0300

    Create custom_text_form_field_widget.dart

---
+++ lib/custom_text_form_field_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  String? hintText;
+  Widget? suffix;
+  bool obscureText;
+  Function(String)? onChanged;
+  CustomTextFormField(
+      {super.key,
+      this.onChanged,
+      this.hintText,
+      this.suffix,
+      required this.obscureText});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: TextFormField(
+        keyboardType: TextInputType.visiblePassword,
+        obscureText: obscureText,
+        // keyboardType: TextInputType.emailAddress,
+        validator: (data) {
+          if (data!.isEmpty) {
+            return 'field if empty ';
+          }
+          return null;
+          // else if (!EmailValidator.validate(data)) {
+          //   return 'invalid';
+          // }
+        },
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          suffixIcon: suffix,
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: Colors.grey,
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.red),
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
